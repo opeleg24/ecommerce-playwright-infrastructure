4ddd65523574bb46156b31c361a821e68cd9da5d
opeleg24 <opeleg1990@gmail.com>
2026-06-18 15:27:12 +0300

    updates to conftest

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -10,17 +10,13 @@ from utilities import base
 from utilities.common_ops import get_data
 
 
+
+
 @pytest.fixture(scope='class')
 def init_page(playwright: Playwright):
-    browser_type = get_data('BROWSER_TYPE').lower()
     slow_motion = int(get_data('SLOW_MODE'))
-    if browser_type == "chrome":
-        base.browser = playwright.chromium.launch(headless=False, channel="chrome", slow_mo=slow_motion)
-    elif browser_type == "firefox":
-        base.browser = playwright.firefox.launch(headless=False, channel="firefox", slow_mo=slow_motion)
-    elif browser_type == "edge":
-        base.browser = playwright.webkit.launch(headless=False, channel="msedge", slow_mo=slow_motion)
-    base.context = base.browser.new_context()
+    base.browser = get_web_driver(playwright, slow_motion)
+    base.context = base.browser.new_context(no_viewport=True)
     # Start tracing before creating a context
     base.context.tracing.start(screenshots=True, snapshots=True, sources=True)
     base.page = base.context.new_page()
@@ -29,8 +25,8 @@ def init_page(playwright: Playwright):
     base.page.goto(get_data('BASE_URL'))
 
     yield
-    # to see results in end of test
-    time.sleep(1)
+    # Results in end of test
+    #time.sleep(1)
 
     base.context.tracing.stop(path="trace.zip")
     base.context.close()
@@ -54,6 +50,57 @@ def refresh_page():
 @pytest.fixture()
 def main_page():
     base.page.goto(get_data('BASE_URL'))
+
+####################################################################################################################
+# Function Name: get_chrome_driver
+# Function Description: Launches a maximized Chrome browser
+# Function Parameters: playwright, slow_motion
+# Function Return: Browser - launched chrome browser
+####################################################################################################################
+def get_chrome_driver(playwright: Playwright, slow_motion: int):
+    return playwright.chromium.launch(headless=False, channel="chrome",
+                                      slow_mo=slow_motion, args=["--start-maximized"])
+
+
+####################################################################################################################
+# Function Name: get_firefox_driver
+# Function Description: Launches a full-screen Firefox browser
+# Function Parameters: playwright, slow_motion
+# Function Return: Browser - launched firefox browser
+####################################################################################################################
+def get_firefox_driver(playwright: Playwright, slow_motion: int):
+    return playwright.firefox.launch(headless=False, channel="firefox",
+                                     slow_mo=slow_motion, args=["--kiosk"])
+
+
+####################################################################################################################
+# Function Name: get_edge_driver
+# Function Description: Launches a maximized Edge browser
+# Function Parameters: playwright, slow_motion
+# Function Return: Browser - launched edge browser
+####################################################################################################################
+def get_edge_driver(playwright: Playwright, slow_motion: int):
+    return playwright.chromium.launch(headless=False, channel="msedge",
+                                      slow_mo=slow_motion, args=["--start-maximized"])
+
+
+####################################################################################################################
+# Function Name: get_web_driver
+# Function Description: Dispatches to the correct browser launcher based on the BROWSER_TYPE config value
+# Function Parameters: playwright, slow_motion
+# Function Return: Browser - launched browser
+####################################################################################################################
+def get_web_driver(playwright: Playwright, slow_motion: int):
+    browser_type = get_data('BROWSER_TYPE').lower()
+    if browser_type == "chrome":
+        return get_chrome_driver(playwright, slow_motion)
+    elif browser_type == "firefox":
+        return get_firefox_driver(playwright, slow_motion)
+    elif browser_type == "edge":
+        return get_edge_driver(playwright, slow_motion)
+    else:
+        raise Exception('Unsupported web driver')
+
 
 # def pytest_exception_interact(node, call, report):
 #     if report.failed:
